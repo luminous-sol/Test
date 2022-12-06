d376ec97dc41ab63a9751b11007ae600c9e18d68
coding.soli <coding.soli@gmail.com>
2022-12-06 07:06:19 +0000

    CVB update

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views # 현재 디렉토리 안에서 import
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page), 
+    path('<int:pk>/',views.PostDetail.as_view()),
+    path('',views.PostList.as_view())
     # url주소/pk(작성된 문서의 순서)/들어오면 view의 single_post_page로 가겠다.
-    path('', views.index), #views 에서 내가 필요한 것을 띄우게 할 수 있다.
+    # path('', views.index), #views 에서 내가 필요한 것을 띄우게 할 수 있다.
+    # path('<int:pk>/', views.single_post_page),
+    # post_list
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,48 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
-# 현재 디렉토리에 있는 modesl에서 Post 불러옴
 
-def index(request): #함수방식
-    
-    posts = Post.objects.all().order_by('-pk') 
-    # order_by('-pk') 최근 작성한 문서가 제일 위에 오도록
-    # DB Query 명령어 => DB에 있는 것 모두 가지고 오게 하는 명령어
-    
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts':posts,
-        }
-    )
-#class Post() #클래스 방식 models.py
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk) # object 값 따라 가는 것 1번이면 1번 따라가고 2번이면 2번따라 가고...
+class PostList(ListView):
+    model = Post
+    ordering = '-pk' #순서대로 작성
     
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post':post,
-        }
-    )
+    
+#    template_name = 'blog/index.html'
 
-# Create your views here.
+class PostDetail(DetailView):
+    model = Post
+    
+
+
+# from django.shortcuts import render
+# from .models import Post
+# # 현재 디렉토리에 있는 modesl에서 Post 불러옴
+
+# def index(request): #함수방식
+    
+#     posts = Post.objects.all().order_by('-pk') 
+#     # order_by('-pk') 최근 작성한 문서가 제일 위에 오도록
+#     # DB Query 명령어 => DB에 있는 것 모두 가지고 오게 하는 명령어
+    
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts':posts,
+#         }
+#     )
+# #class Post() #클래스 방식 models.py
+
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk) # object 값 따라 가는 것 1번이면 1번 따라가고 2번이면 2번따라 가고...
+    
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post':post,
+#         }
+#     )
+
+# # Create your views here.
